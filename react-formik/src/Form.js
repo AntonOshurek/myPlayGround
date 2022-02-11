c8b6e6133e8721b672e35d0ec78ca79f10ced32e
AntonOshurek <oshurekweb@gmail.com>
2022-02-11 22:35:03 +0100

    add basic formik logic

--- react-formik/src/Form.js
+++ react-formik/src/Form.js
@@ -1,46 +1,55 @@
 import { Formik } from 'formik';
 import { useFormik } from 'formik';
 
-const formik = useFormik({
-  initialValues: {
-    Name: '',
-    email: '',
-    amount: '',
-    currency: '',
-    text: '',
-    terms: false
-  },
-  onSubmit: values => {
-    console.log(JSON.stringify(values, null, 2));
-  },
-});
-
 const Form = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      Name: '',
+      email: '',
+      amount: 0,
+      currency: '',
+      text: '',
+      terms: false
+    },
+    onSubmit: values => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={formik.handleSubmit}>
       <h2 className="form__title">Отправить пожертвование</h2>
       <label className="form__label" htmlFor="name">Ваше имя</label>
       <input className="form__input"
         id="name"
         name="name"
         type="text"
+        value={formik.values.name}
+        onChange={formik.handleChange}
       />
       <label className="form__label" htmlFor="email">Ваша почта</label>
       <input className="form__input"
         id="email"
         name="email"
         type="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
       />
       <label className="form__label" htmlFor="amount">Количество</label>
       <input className="form__input"
         id="amount"
         name="amount"
         type="number"
+        value={formik.values.amount}
+        onChange={formik.handleChange}
       />
       <label className="form__label" htmlFor="currency">Валюта</label>
       <select className="form__select"
         id="currency"
-        name="currency">
+        name="currency"
+        value={formik.values.currency}
+        onChange={formik.handleChange}>
           <option value="">Выберите валюту</option>
           <option value="USD">USD</option>
           <option value="UAH">UAH</option>
@@ -50,9 +59,15 @@ const Form = () => {
       <textarea className="form__textarea"
         id="text"
         name="text"
+        value={formik.values.text}
+        onChange={formik.handleChange}
       />
       <label className="form__label form__label--checkbox">
-        <input className="form__checkbox" name="terms" type="checkbox" />
+        <input className="form__checkbox"
+        name="terms"
+        type="checkbox"
+        value={formik.values.terms}
+        onChange={formik.handleChange} />
         Соглашаетесь с политикой конфиденциальности?
       </label>
       <button className="form__submit" type="submit">Отправить</button>
