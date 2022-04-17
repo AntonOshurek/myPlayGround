4a8fed2059509ac0c1ada792285640d96c80408d
AntonOshurek <oshurekweb@gmail.com>
2022-04-17 21:52:15 +0200

    add avatar lib

--- dice-avatart/source/scripts/avatar.js
+++ dice-avatart/source/scripts/avatar.js
@@ -1,3 +1,28 @@
+// import { createAvatar } from '@dicebear/avatars';
+import { createAvatar } from '@dicebear/avatars';
+import * as style from '@dicebear/adventurer';
+// @dicebear/big-ears-neutral
+// @dicebear/avatars-identicon-sprites
+// @dicebear/adventurer
+
 export default function avatar() {
-  console.log('avatar');
+  const avatarBlock = document.querySelector('.form__avatar');
+  const nameInput = document.querySelector('.form__input');
+
+  const createAva = (text) => {
+    let svg = createAvatar(style, {
+      seed: `${text}`,
+      backgroundColor: 'tomato'
+      // ... and other options
+    });
+    return svg;
+  };
+
+  nameInput.addEventListener('input', (evt) => {
+    evt.preventDefault();
+    const avatarName = evt.target.value;
+    const avatarSvg = createAva(avatarName);
+
+    avatarBlock.innerHTML = avatarSvg;
+  })
 }
