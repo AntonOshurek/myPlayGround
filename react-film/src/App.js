6931912dc07aecca8599bde15361e4860474266b
AntonOshurek <oshurekweb@gmail.com>
2022-07-08 21:28:33 +0200

    add footer and main

--- react-film/src/App.js
+++ react-film/src/App.js
@@ -1,9 +1,13 @@
 import { Header } from './layout/header';
+import { Main } from './layout/Main';
+import { Footer } from './layout/footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
