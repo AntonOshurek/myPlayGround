e29d0b4976f35b177dd5533f43bb780f3afcf60c
AntonOshurek <oshurekweb@gmail.com>
2022-07-23 19:48:54 +0200

    get data

--- react-shop-project/src/components/Header.jsx
+++ react-shop-project/src/components/Header.jsx
@@ -5,13 +5,13 @@ export const Header = () => {
   return(
     <header className='header'>
       <nav>
-        <div class="nav-wrapper navigation container">
-          <a href="#!" class="brand-logo"><i class="material-icons">shop</i>React shop</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-            <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-            <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-            <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+        <div className="nav-wrapper navigation container">
+          <a href="#!" className="brand-logo"><i className="material-icons">shop</i>React shop</a>
+          <ul className="right hide-on-med-and-down">
+            <li><a href="sass.html"><i className="material-icons">search</i></a></li>
+            <li><a href="badges.html"><i className="material-icons">view_module</i></a></li>
+            <li><a href="collapsible.html"><i className="material-icons">refresh</i></a></li>
+            <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
           </ul>
         </div>
       </nav>
--- react-shop-project/src/components/Shop.jsx
+++ react-shop-project/src/components/Shop.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import {API_KEY, API_URL} from '../config';
+
+import { Preloader } from './Preloader';
+
 import './shop.css';
 
 export const Shop = () => {
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        'Authorization': API_KEY,
+      },
+    }).then(response => response.json())
+    .then((data) => {
+      data.featured && setGoods(data.shop);
+      console.log(data.shop)
+      setLoading(false);
+    })
+  }, []);
+
   return(
     <section className="shop container">
       <h2>Shop</h2>
+
+      {
+        loading ? <Preloader /> : 'Goods'
+      }
     </section>
   )
 };
