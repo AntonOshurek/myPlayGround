5ae902f82ac03816e4bc44bdccbe48e9cb040c26
AntonOshurek <oshurekweb@gmail.com>
2022-07-30 14:13:45 +0200

    add total price in cart

--- react-shop-project/src/components/cart/CartList.jsx
+++ react-shop-project/src/components/cart/CartList.jsx
@@ -4,6 +4,10 @@ import { CartItem } from "./CartItem";
 export const CartList = (props) => {
   const { order = [], handleCartShow } = props;
 
+  const totalPrice = order.reduce((sum, el) => {
+    return sum + el.price.finalPrice * el.quantity;
+  }, 0)
+
   return(
     <ul className="collection cart-list">
       <li className="collection-item active">Корзина</li>
@@ -12,7 +16,7 @@ export const CartList = (props) => {
           <CartItem key={item.mainId} {...item}/>
         )) : (<li className="collection-item">Корзина пуста</li>)
       }
-      <li className="collection-item">Общая стоимость</li>
+      <li className="collection-item active">Общая стоимость = {totalPrice} руб.</li>
       <button className='cart-list__close-btn' onClick={handleCartShow}>
         <i className="material-icons">close</i>
       </button>
